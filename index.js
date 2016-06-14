cbfcd91876fe61cbf4bf8e467d74f0ebe41c8600
Colin <colinwillson@gmail.com>
2016-06-14 16:17:26 -0700

    Realized I was trying to send an object over the HttpRequest. Converting to a JSON first now.

--- index.js
+++ index.js
@@ -48,32 +48,33 @@ function getData(URL) {
 
   console.log("getting data for: ", URL);
 
-  // jsdom.env({url: URL, scripts: ["http://code.jquery.com/jquery.js"], created: function(err, window) {
-  //   console.log("created: " + err);
-  //
-  // }, done: function (err, window) {
-  //   // free memory associated with the window
-  //   //console.log("window: " + window);
-  //
-  //   if (!err && window !== null) {
-  //     //var keyWordsArray = getKeyWords(window);
-  //     //var parsedWordsArray = parseWords(keyWordsArray);
-  //     //var countedWords = countKeyWords(parsedWordsArray);
-  //
-  //     //var convertedName = site.hostname.replace(/\./g, " ");
-  //     // ref.child('websites/' + convertedName + "/pages").push({page: site.page.val().URL, keyWords: countedWords});
-  //     //fireUpload("websites/" + convertedName + "/pages", "push", {page: site.page.URL, keyWords: countedWords});
-  //     var MPN = window.$('#divManufacturerPartNum').text();
-  //     var descrption = window.$('#divDes').text();
-  //     // var MPN = window.getElementById('divManufacturerPartNum');
-  //     console.log(MPN);
-  //     return {MPN: MPN, descrption: descrption};
-  //
-  //
-  //   } else {
-  //       console.log(err + "- invalid url.");
-  //   }
-  // }});
+  jsdom.env({url: URL, scripts: ["http://code.jquery.com/jquery.js"], created: function(err, window) {
+    console.log("created: " + err);
+
+  }, done: function (err, window) {
+    // free memory associated with the window
+    //console.log("window: " + window);
+
+    if (!err && window !== null) {
+      //var keyWordsArray = getKeyWords(window);
+      //var parsedWordsArray = parseWords(keyWordsArray);
+      //var countedWords = countKeyWords(parsedWordsArray);
+
+      //var convertedName = site.hostname.replace(/\./g, " ");
+      // ref.child('websites/' + convertedName + "/pages").push({page: site.page.val().URL, keyWords: countedWords});
+      //fireUpload("websites/" + convertedName + "/pages", "push", {page: site.page.URL, keyWords: countedWords});
+      var MPN = window.$('#divManufacturerPartNum').text();
+      var descrption = window.$('#divDes').text();
+      // var MPN = window.getElementById('divManufacturerPartNum');
+      console.log(MPN);
+      var returnJSON = JSON.stringify({MPN: MPN, descrption: descrption})
+      return returnJSON;
+
+
+    } else {
+        console.log(err + "- invalid url.");
+    }
+  }});
 }
 
 // function MouserData(URL) {
